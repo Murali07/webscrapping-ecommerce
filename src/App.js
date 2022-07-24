211c6eaef6e6b72c6ae37f41d6ca23c3e93f409d
Murali07 <muraliselvaraj97@gmail.com>
2022-07-24 18:45:34 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 // import { Routes, Route, Link, useNavigate } from "react-router-dom";
-// import { useState } from "react";
+import { useState } from "react";
 // import { Navigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 // import InfoIcon from '@mui/icons-material/Info';
-// import IconButton from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 // import { ThemeProvider, createTheme } from '@mui/material/styles';
+import JSONDATA from "./mockdata.json";
+import SearchIcon from '@mui/icons-material/Search';
 
 // function App() {
 
@@ -47,6 +49,7 @@ import HomeIcon from '@mui/icons-material/Home';
 
 const mobiles = [
   {
+    id: "1", 
     img: "https://rukminim1.flixcart.com/image/312/312/l1dwknk0/mobile/f/t/x/-original-imagcyj4unsxhadw.jpeg?q=70",
     title: "realme C31 (Light Silver, 32 GB)",
     rating: "4.6",
@@ -54,6 +57,8 @@ const mobiles = [
     final_price: "8,999"
   },
   {
+
+    id: "2", 
     img: "https://rukminim1.flixcart.com/image/312/312/ku4ezrk0/mobile/p/e/4/c31-mzb0a0jin-poco-original-imag7bzqxgdhgf2n.jpeg?q=70",
     title: "POCO C31 (Royal Blue, 64 GB)",
     rating: "4.4",
@@ -61,6 +66,7 @@ const mobiles = [
     final_price: "7,999"
   },
   {
+    id: "3", 
     img: "https://rukminim1.flixcart.com/image/312/312/l4n2oi80/mobile/x/o/a/-original-imagfhu75eupxyft.jpeg?q=70",
     title: "SAMSUNG Galaxy F13 (Waterfall Blue, 64 GB)",
     rating: "4.1",
@@ -68,6 +74,7 @@ const mobiles = [
     final_price: "11,999"
   },
   {
+    id: "4", 
     img: "https://rukminim1.flixcart.com/image/312/312/kg8avm80/mobile/r/h/z/apple-iphone-12-dummyapplefsn-original-imafwg8duby8qbn4.jpeg?q=70",
     title: "APPLE iPhone 12 (Black, 128 GB)",
     rating: "4.6",
@@ -75,6 +82,7 @@ const mobiles = [
     final_price: "56,999"
   },
  {
+    id: "5", 
     img: "https://m.media-amazon.com/images/I/81OZGH4fZiL._SX522_.jpg",
     title: "Redmi 10 Power (Sporty Orange, 8GB RAM, 128GB Storage)",
     rating: "3.6",
@@ -82,6 +90,7 @@ const mobiles = [
     final_price: "14,999"
   },
   {
+    id: "6", 
     img: "https://m.media-amazon.com/images/I/71geVdy6-OS._SX522_.jpg",
     title: "OPPO A74 5G (Fantastic Purple,6GB RAM,128GB Storage)",
     rating: "4.2",
@@ -89,6 +98,7 @@ const mobiles = [
     final_price: "14,990"
   },
  {
+    id: "7", 
     img: "https://m.media-amazon.com/images/I/61+Q6Rh3OQL._SX522_.jpg",
     title: "OnePlus Nord CE 2 5G (Bahamas Blue, 8GB RAM, 128GB Storage)",
     rating: "4.3",
@@ -96,6 +106,7 @@ const mobiles = [
     final_price: "24,999"
   },
   {
+    id: "8", 
     img: "https://m.media-amazon.com/images/I/71BZ2hz0IZL._SX522_.jpg",
     title: "Nokia G21 Android Smartphone",
     rating: "4.1",
@@ -103,6 +114,7 @@ const mobiles = [
     final_price: "11,999"
   },
  {
+    id: "9", 
     img: "https://m.media-amazon.com/images/I/41+sLJHyA8L._SY300_SX300_.jpg",
     title: "Vivo Y21G (Midnight Blue, 4GB RAM, 64GB ROM)",
     rating: "4",
@@ -110,6 +122,7 @@ const mobiles = [
     final_price: "13,499"
   },
  {
+    id: "10", 
     img: "https://m.media-amazon.com/images/I/413iZjGV3cL._SX522_.jpg",
     title: "Lava Agni 5G |64 MP AI Quad Camera| (8GB RAM/128 GB ROM)",
     rating: "4.3",
@@ -118,10 +131,11 @@ const mobiles = [
   },
 ]
 
-
 function App(){  
 
   // const navigate = useNavigate();
+
+  const [searchText, setSearchText] = useState("");
 
   return (
     <div className='App'>
@@ -130,10 +144,44 @@ function App(){
         <Toolbar>
           <Button color="inherit"><HomeIcon></HomeIcon>Home</Button>
           <Button color="inherit">Electronics</Button>
-          <Button color="inherit">Fashion</Button>          
+          <Button color="inherit">Fashion</Button>  
+               
 
         </Toolbar>
       </AppBar>
+
+      <div>
+
+        
+        <input className='input' 
+          type="text" 
+          placeholder='Search..' 
+          onChange={ event => {setSearchText(event.target.value)} }>                   
+           
+        </input> 
+
+        <IconButton><SearchIcon></SearchIcon></IconButton>                  
+        
+
+        {JSONDATA.filter((val) => {
+          if(searchText === ""){
+            return val
+          }
+          else if(val.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())){
+            return val
+          }
+
+        }).map((val, key) => {
+            return (
+              <div className='product' key={key}>
+                <p>{val.title}</p>
+              </div>
+            )
+          })}
+
+      </div>
+
+      
       
       <div className='phone-list-container'>
         {mobiles.map((mb) => (
